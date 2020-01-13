8969dbfbf5eeb8b81f151d0f513eda0171fa0f58
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2020-01-13 15:47:35 +1100

    first commit of project with desktop and mobile version

---
+++ base-apparel/index.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+
+function getInputValue() {
+    const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    const inputVal = document.getElementById("email").value;
+
+    if (expression.test(inputVal)) {
+        const msg = document.getElementById("msg-label").innerText = "Submit Successfully!";
+        document.getElementById('msg-label').style.color = "rgb(6, 107, 1)";
+        document.getElementsByClassName('icon-error')[0].style.display = 'none';
+    } else {
+        const msgErro = document.getElementById("msg-label").innerText = "Please provide a valid email";
+        document.getElementById('msg-label').style.color = "red";
+        document.getElementsByClassName('icon-error')[0].style.display = 'block';
+    }
+}
